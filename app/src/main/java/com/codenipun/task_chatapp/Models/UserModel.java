0b86b773304df1cfd74dd62965c8bc12931de990
nipunjain <nipunjain119@gmail.com>
2021-11-27 17:33:06 +0530

    Initial commit

---
+++ app/src/main/java/com/codenipun/task_chatapp/Models/UserModel.java
@@ -0,0 +1,37 @@
+package com.codenipun.task_chatapp.Models;
+
+public class UserModel {
+    String Uname;
+    String Uid;
+    String profile;
+
+    public UserModel(String uname, String uid, String profile) {
+        Uname = uname;
+        Uid = uid;
+        this.profile = profile;
+    }
+
+    public String getUname() {
+        return Uname;
+    }
+
+    public void setUname(String uname) {
+        Uname = uname;
+    }
+
+    public String getUid() {
+        return Uid;
+    }
+
+    public void setUid(String uid) {
+        Uid = uid;
+    }
+
+    public String getProfile() {
+        return profile;
+    }
+
+    public void setProfile(String profile) {
+        this.profile = profile;
+    }
+}
